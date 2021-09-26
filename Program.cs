8e70a213e8f1e9f123a493b177c72e185a5acfe8
Blake Shaw <blake.shaw@gmail.com>
2021-09-25 21:39:21 -0600

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Primes
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			for(uint i = 1; i <= 1000; i++) {
+				if(Primes.IsPrime(i)) Console.WriteLine(i);
+			}
+		}
+	}
+
+	class Primes
+	{
+		public static bool IsPrime(uint x)
+		{
+			if(x < 2) return false;
+			uint upperBound = (uint)Math.Sqrt(x) + 1;
+			for(uint i = 2; i < upperBound; i++) {
+				if(x % i == 0) return false;
+			}
+			return true;
+		}
+	}
+}
